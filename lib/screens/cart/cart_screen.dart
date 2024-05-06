8e2140f9764aedb48e7df220da66f9b500bffe14
Aqsal Dharmaputra <aqsaldpa28@gmail.com>
2024-05-06 09:29:17 +0700

    try riverpod generated

---
+++ lib/screens/cart/cart_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/cart_provider.dart';
+
+class CartScreen extends ConsumerStatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  ConsumerState<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends ConsumerState<CartScreen> {
+  bool showCoupon = true;
+
+  @override
+  Widget build(BuildContext context) {
+    final cartProducts = ref.watch(cartNotifierProvider);
+    final total = ref.watch(cartTotalProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+        centerTitle: true,
+        // actions: [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          children: [
+            Column(
+                children: cartProducts.map((prod) {
+              return Container(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      prod.image,
+                      width: 60,
+                      height: 60,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(prod.title),
+                    const Expanded(child: SizedBox()),
+                    Text("\$ ${prod.price}"),
+                  ],
+                ),
+              );
+            }).toList()),
+            Text("Total Price  - \$ $total")
+          ],
+        ),
+      ),
+    );
+  }
+}
